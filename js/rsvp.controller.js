4bb981722e07c12341bea1c25361297666d0dc44
mattk91 <Xci6pzb7>
2016-04-24 16:32:58 +0100

    Fix email

--- js/rsvp.controller.js
+++ js/rsvp.controller.js
@@ -9,7 +9,7 @@
     function RsvpController($scope, $http, $httpParamSerializerJQLike) {
         var mailerUrl = 'mail/submit.php';
         var successContainsStr = 'Thanks! We\'ve received your information';
-        $scope.email = 'matt91@gmail.com';
+        $scope.email = 'mattk91@gmail.com';
         $scope.submitted = false;
         $scope.processing = false;
         $scope.formData = {
