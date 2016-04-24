4e01e2f3d618a1b44a2f10979e148fdd0bb162f5
mattk91 <Xci6pzb7>
2016-04-24 15:03:30 +0100

    Change form rsvp

--- js/rsvp.controller.js
+++ js/rsvp.controller.js
@@ -8,10 +8,14 @@
     RsvpController.$inject = ['$scope', '$rootScope', '$http'];
     function RsvpController($scope, $rootScope, $http) {
         var mailerUrl = 'mail/submit.php';
+        $scope.formData = {};
+
         $scope.submitRsvp = function() {
-            $http.post(mailerUrl, RsvpForm, {headers : {
+            $http.post(mailerUrl, $scope.formData, {headers : {
                 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
             });
-        }
+        };
+
+        // todo custom parse response as it always shows a 200 even if there's an error ....
     }
 })();
