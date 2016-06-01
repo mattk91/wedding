8aea847211901fd55601123864b55c5090e35bd7
mattk91 <Xci6pzb7>
2016-06-01 18:26:33 +0100

    Make navbar close when clicked

--- js/navbar.controller.js
+++ js/navbar.controller.js
@@ -5,6 +5,13 @@
         .module('app')
         .controller('NavBarController', NavBarController);
 
-    function NavBarController() {
+    NavBarController.$inject = ['$scope'];
+    function NavBarController($scope) {
+        $('.navbar .nav li a').click(function() {
+            var navbar_toggle = $('.navbar-toggle');
+            if (navbar_toggle.is(':visible')) {
+                navbar_toggle.trigger('click');
+            }
+        });
     }
 })();
